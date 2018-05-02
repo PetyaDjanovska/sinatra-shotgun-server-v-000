7963accd4da2c98295ba96067d976b9084dc82e0
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-02 18:45:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,9 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! This is Sinatra start.\n
-    Now let's check 'shotgun'  Tratatata \r
-    Will this be on a new line?"
+    "Here we go..."
   end
 
 end
